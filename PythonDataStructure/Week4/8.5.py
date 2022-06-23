c2b710894750ac39fe60680b227437a359721fa8
chkvdm <vadikvr6@outlook.com>
2022-06-23 17:05:19 +0400

    Remade 8.5

--- PythonDataStructure/Week4/8.5.py
+++ PythonDataStructure/Week4/8.5.py
@@ -4,8 +4,11 @@ if len(fname) < 1:
 fh = open(fname)
 count = 0
 for line in fh:
-    if not line.startswith('From '): continue
-    count += 1
+    if not line.startswith('From '):
+        continue
     word = line.split()
+    if len(word) < 2:
+        continue
+    count += 1
     print (word[1])
 print("There were", count, "lines in the file with From as the first word")
