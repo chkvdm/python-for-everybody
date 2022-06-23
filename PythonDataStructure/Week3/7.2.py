cdda60e18f28e25ce2cbff7b9e7f9785cf682890
chkvdm <vadikvr6@outlook.com>
2022-06-23 12:18:33 +0400

    Add 7.2

---
+++ PythonDataStructure/Week3/7.2.py
@@ -0,0 +1,11 @@
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+count = 0
+val = 0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    count += 1
+    val += float(line[line.find('0'): ])
+print('Average spam confidence: ' + str(val / count))
