ed3c2f3e2475cc71f5fb644aba94b3a3b5a235fc
chkvdm <vadikvr6@outlook.com>
2022-07-01 11:41:05 +0400

    ExtractingDataWithRegularExpressions

---
+++ UsingPythontoAccessWebData/Week2/ExtractingDataWithRegularExpressions.py
@@ -0,0 +1,18 @@
+import re
+
+# version_1
+
+file = open('/Users/vadim/Documents/PythonForEverybody/UsingPythontoAccessWebData/Week2/regex_sum_1584015.txt')
+nums = list()
+text = file.read()
+number = re.findall('[0-9]+', text)
+for num in number:
+    num = float(num)
+    nums.append(num)
+print (sum(nums))
+
+
+# version_2
+
+file = open('/Users/vadim/Documents/PythonForEverybody/UsingPythontoAccessWebData/Week2/regex_sum_1584015.txt')
+print (sum([float(num) for num in re.findall('[0-9]+', file.read())]))
