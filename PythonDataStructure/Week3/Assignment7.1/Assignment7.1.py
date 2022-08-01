a558393d8d77e3fd7d8d528e38c9d49b8bec3bcf
chkvdm <vadikvr6@outlook.com>
2022-08-01 18:09:19 +0400

    Assignment 7.1

--- PythonDataStructure/Week3/Assignment7.1/Assignment7.1.py
+++ PythonDataStructure/Week3/Assignment7.1/Assignment7.1.py
@@ -1,5 +1,8 @@
 # Use words.txt as the file name
+
 fname = input("Enter file name: ")
+# /Users/...../Assignment7.1/words.txt
+
 fh = open(fname)
 for line in fh:
     print ((line.rstrip()).upper())
