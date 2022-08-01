91cd9db92bb8b6df687163d401f40e3bfe0d0ede
chkvdm <vadikvr6@outlook.com>
2022-08-01 23:49:56 +0400

    Assignment 9.4

---
+++ PythonDataStructure/Week5/Assignment9.4.py
@@ -0,0 +1,22 @@
+name = input("Enter file: ")
+# File name: mbox-short.txt.
+# The file is not in the list because the assignment was run online. 
+# Downloading the file is not provided by the condition.
+
+handle = open(name)
+from_val = dict()
+for line in handle:
+    if not line.startswith('From '):
+        continue
+    words = line.split()
+    if len(word) > 2:
+        from_val[words[1]] = from_val.get(words[1], 0) + 1
+
+most_sender = " "
+most_val = 0
+for key,val in from_val.items():
+    if val > most_val:
+        most_sender = key
+        most_val = val
+    
+print(most_sender, most_val)
