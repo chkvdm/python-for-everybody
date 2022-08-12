8bb7dfcbc01834becb97a2f7702d9c3ee3be8f5a
chkvdm <vadikvr6@outlook.com>
2022-08-12 14:08:31 +0400

    Multi-Table Database - Tracks

--- UsingDatabasesWithPython/Week3/Multi-TableDatabaseTracks.py
+++ UsingDatabasesWithPython/Week3/Multi-TableDatabaseTracks.py
@@ -39,7 +39,7 @@ CREATE TABLE Track (
 
 
 fname = input('Enter file name: ')
-if ( len(fname) < 1 ) : fname = 'Library.xml'
+# Library.xml
 
 # <key>Track ID</key><integer>369</integer>
 # <key>Name</key><string>Another One Bites The Dust</string>
